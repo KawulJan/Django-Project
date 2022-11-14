a2f02a526b18126fe3322f723c20021f7c2e73ed
KawulJan <aytoldi.uii@gmail.com>
2022-11-14 18:40:15 +0900

    django projects

---
+++ website/web/views.py
@@ -0,0 +1,34 @@
+
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"] = "Uyghurjan"
+        return ctxt
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self):
+       ctxt = super().get_context_data()
+       ctxt["num_services"] = 67565441
+       ctxt["skills"] =[
+        "Python",
+        "HTML",
+        "JavaScript",
+        "Java",
+        "CSS",
+
+       ]
+       return ctxt
+
+
+
+class ContectView(TemplateView):
+    template_name = "contect.html"
